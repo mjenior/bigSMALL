64b3780a5c72e0d462451e95ce4227a42f68acba
Matt Jenior <mljenior@umich.edu>
2016-06-29 16:16:37 -0400

    changed algorithm back from quotient to subtraction

--- bipartite_graph.py
+++ bipartite_graph.py
@@ -309,17 +309,21 @@ def calculate_score(compound_transcript_dict, compound_degree_dict, compound_nam
 			input_score = 0.0
 		else:
 			input_score = input_transcription / outdegree
+
 		if indegree == 0.0:
-			output_score = 0.01
-		elif output_transcription == 0.0:
-			output_score = 0.01
+			output_score = 0.0
 		else:
 			output_score = output_transcription / indegree
 		
-		score_quotient = input_score / output_score
-		if score_quotient == 0.0: score_quotient = 1.0
-		
-		final_score = math.log(score_quotient, 2)
+		score_difference = input_score - output_score
+
+		if score_difference < 1 and score_difference > -1:
+			final_score = 0.0
+		elif score_difference <= -1:
+			final_score = math.log(abs(score_difference), 2) * -1
+		else:
+			final_score = math.log(score_difference, 2)
+
 		final_score = float("%.3f" % final_score)
 
 		score_dict[compound].extend((compound_name, final_score))
